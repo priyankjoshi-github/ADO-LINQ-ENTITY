8b10e7886564e29637c0caf37b60b16b4ddbbeee
Priyankjoshi-github <102507819+priyankjoshi-github@users.noreply.github.com>
2022-04-21 01:53:25 +0530

    Add files via upload

---
+++ Sql Football Query.sql
@@ -0,0 +1,43 @@
+Create database Table;
+
+create table FootBallLeague
+(
+MatchId int Identity(1001,1) primary key,
+TeamName1 nvarchar(50) ,
+TeamName2 nvarchar(50) ,
+MatchStatus nvarchar(50) Not NULL,
+WinningTeam nvarchar(50),
+Points int NOT NULL
+
+);
+
+
+
+
+INSERT INTO FootBallLeague(TeamName1,TeamName2,MatchStatus,WinningTeam,Points)
+VALUES
+('Italy','France','Win','France',4),
+('Brazil','Portugal','Draw',null,2),
+('England','Japan','Win','England',4),
+('Usa','Russia','Win','Russia',4),
+('England','Russia','Win','Russia',4),
+('Japan','USA','Draw',null,2);
+('Portugal','Italy','Draw',null,2),
+('Brazil','France','Win','Brazil',4),
+
+
+select * from FootBallLeague
+
+Create proc InsertDetails
+@TeamName1 nvarchar(50),
+@TeamName2 nvarchar(50),
+@Status nvarchar(50),
+@WinningTeam nvarchar(50),
+@points int
+as
+begin
+insert into FootBallLeague
+(TeamName1,TeamName2,MatchStatus,WinningTeam,Points)
+values(@TeamName1,@TeamName2,@Status,@WinningTeam,@points)
+end
+go
